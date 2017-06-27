c79841ab6ed44c029a163e2ea8b2a3e323d4772f
happyinc <app@happyhappyinc.com>
2017-06-27 10:15:25 -0500

    Se configura plantilla
    Se realiza organizacion de la plantilla, separando los elementos de pie de pagina,cabecera y menu del espacion de trabajo "body"

---
+++ happyprucon/app/src/admin_2_material_design/footer.php
@@ -0,0 +1,10 @@
+ <!-- BEGIN FOOTER -->
+        <div class="page-footer">
+            <div class="page-footer-inner"> 2017 &copy; Happy Happy INC
+                <a target="_blank" href="http://www.happyhappyinc.com">Happy Happy INC</a> &nbsp;|&nbsp;
+                <div class="scroll-to-top">
+                    <i class="icon-arrow-up"></i>
+                </div>
+            </div>
+		</div>
+<!-- END FOOTER -->
