48b9808d9983fa2b726ef708ec82737997a74eb1
happyinc <app@happyhappyinc.com>
2017-08-10 11:00:18 -0500

    cambios e inicio

--- happyprucon/app/src/geo.php
+++ happyprucon/app/src/geo.php
@@ -1,3 +1,47 @@
+<?php
+error_reporting(0);
+require_once'../../externo/plugins/PDOModel.php';
+include '../class/sessions.php';
+
+$objSe = new Sessions();
+$objSe->init();
+
+$usu_id = isset($_SESSION['id_usuario']) ? $_SESSION['id_usuario'] : null ;
+$rol = isset($_SESSION['id_roles']) ? $_SESSION['id_roles'] : null ;
+$fullname = isset($_SESSION['nombre_completo']) ? $_SESSION['nombre_completo']:null;
+$name = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : null ;
+$lastname = isset($_SESSION['apellido']) ? $_SESSION['apellido'] : null ;
+$genero = isset($_SESSION['genero']) ? $_SESSION['genero'] : null ;
+$tel = isset($_SESSION['telefono']) ? $_SESSION['telefono'] : null ;
+$correo = isset($_SESSION['correo']) ? $_SESSION['correo'] : null ;
+$meta = isset($_SESSION['suenos']) ? $_SESSION['suenos'] : null ;
+
+$id_user = "";
+
+if(isset($_POST["id_usuario"]) && $_POST["id_usuario"] != "")
+{
+    $id_user = $_POST["id_usuario"];
+}
+elseif(isset($_GET["id_usuario"]) && $_GET["id_usuario"] != "")
+{
+    $id_user = $_GET["id_usuario"];
+}
+$objUbicacion = new PDOModel();
+$objUbicacion->where("id_usuario", $id_user);
+$res_usuarios =  $objUbicacion->select("usuarios");
+foreach ($res_usuarios as $usuarios)
+{
+    $rol = $usuarios["rol"] ;
+    $fullname = $usuarios["fullname"] ;
+    $name = $usuarios["name"] ;
+    $lastname = $usuarios["lastname"] ;
+    $genero = $usuarios["genero"] ;
+    $tel = $usuarios["tel"] ;
+    $correo = $usuarios["correo"] ;
+}
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,6 +61,36 @@
             padding: 0;
         }
     </style>
+    <script type="text/javascript">
+<?
+$objConn = new PDOModel();
+$objConn->where("id_roles", 2);
+$result =  $objConn->select("usuarios");
+
+?>
+        function init() {
+
+            var latlng = new google.maps.LatLng(<?php echo $result[0]['latitud']; ?>, <?php echo $result[0]['longitud']; ?>);
+
+            var myOptions = {
+                zoom: 15,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+            sucursal = new google.maps.Marker({
+                position: latlng,
+                icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                map: map
+            });
+
+        }
+
+        window.onload = init;
+
+    </script>
 </head>
 <body>
 <div id="map"></div>
