3d8228078ec90d816cc0772c09bb1481eb4b1258
DESARROLLO HAPPY INC <happy09127@gmail.com>
2017-08-16 07:22:07 -0500

    domicilios

---
+++ happyprucon/app/src/domicilios.php
@@ -0,0 +1,233 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DESARROLLO HAPPY INC
+ * Date: 11/08/2017
+ * Time: 10:53 AM
+ */
+
+?>
+
+<html>
+
+<head>
+    <?php
+    include "include_css.php";
+    include "funciones.php";
+    require_once '../../externo/plugins/PDOModel.php';
+    ?>
+</head>
+
+<body class="page-header-fixed page
+-sidebar-closed-hide-logo page-container-bg-solid page-md">
+
+<div class="page-content-wrapper">
+    <!-- BEGIN CONTENT BODY -->
+    <div class="page-content">
+
+        <div class="row">
+
+            <div class="col-lg-6 col-xs-12 col-sm-12">
+                <!-- DISEÑO DE UN DIV -->
+                <div class="portlet light ">
+
+                    <!-- cabezera del div blanco PEDIDOS SIN ASIGNAR -->
+                    <div class="portlet-title tabbable-line">
+                        <div class="caption">
+                            <i class="icon-bubbles font-dark hide"></i>
+                            <span class="caption-subject font-dark bold uppercase">Pedidos Sin Asignar</span>
+                        </div>
+                    </div>
+                    <!-- fin cabezera del div blanco PEDIDOS SIN ASIGNAR -->
+
+                    <!-- cuerpo del div blanco PEDIDOS SIN ASIGNAR -->
+                    <div class="portlet-body">
+
+                        <div class="tab-content">
+
+                            <div class="scroller" style="height: 338px;" data-always-visible="1" data-rail-visible1="0"
+                                 data-handle-color="#D7DCE2">
+
+                                <div>
+
+                                    <!-- Seccion de todos los comentarios -->
+                                    <div class="mt-comments">
+                                        <?
+                                        //informacion del pedido
+                                        $objPedido = new PDOModel();
+                                        /*$objPedido->andOrOperator = "AND";
+                                        $objPedido->where("id_estado", 7);
+                                        $objPedido->where("forma_adquisicion", 1);*/
+                                        $result = $objPedido->select("pedido");
+
+                                        foreach ($result as $pedido) {
+
+                                            ?>
+                                            <small>
+                                                <!-- diseño de una fila de comentario -->
+                                                <div class="mt-comment">
+
+                                                    <div class="mt-comment-img">
+                                                        <img src="../assets/pages/media/users/avatar1.jpg"/>
+                                                        <b>PEDIDO N° <? echo $pedido["id_producto"] ?></b>
+                                                    </div>
+                                                    <div class="mt-comment-body">
+                                                        <div class="mt-comment-info">
+                                                            <span class="mt-comment-author">Leidy Ramirez</span>
+                                                            <span class="mt-comment-date">11 Agus, 10:30AM</span>
+                                                        </div>
+                                                        <div class="mt-comment-text"><b><? echo $pedido["cantidad"] ?></b> - <? echo nombre_producto($pedido["id_producto"])?>
+                                                        </div>
+                                                        <div class="mt-comment-details">
+                                                            <span class="mt-comment-status mt-comment-status-rejected">Pendiente</span>
+                                                            <ul class="mt-comment-actions">
+                                                                <li>
+                                                                    <a href="#">Asignar</a>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                <!-- fin diseño de una fila de comentario -->
+                                            </small>
+                                            <?
+                                        }
+
+
+                                        ?>
+
+
+                                    </div>
+                                    <!-- fin Seccion de todos los comentarios -->
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!-- fin cuerpo del div blanco PEDIDOS SIN ASIGNAR -->
+
+                </div>
+                <!-- FIN DISEÑO DE UN DIV -->
+
+
+                <!-- DISEÑO DE UN DIV -->
+                <div class="portlet light ">
+
+                    <!-- cabezera del div blanco PEDIDOS SIN ASIGNAR -->
+                    <div class="portlet-title tabbable-line">
+                        <div class="caption">
+                            <i class="icon-bubbles font-dark hide"></i>
+                            <span class="caption-subject font-dark bold uppercase">Pedidos Rechazados</span>
+                        </div>
+                        <ul class="nav nav-tabs">
+                            <li class="active">
+                                <a href="#portlet_comments_1" data-toggle="tab"> Pending </a>
+                            </li>
+                            <li>
+                                <a href="#portlet_comments_2" data-toggle="tab"> Approved </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- fin cabezera del div blanco PEDIDOS SIN ASIGNAR -->
+
+                    <!-- cuerpo del div blanco PEDIDOS SIN ASIGNAR -->
+                    <div class="portlet-body">
+
+                        <div class="tab-content">
+
+                            <div class="scroller" style="height: 338px;" data-always-visible="1" data-rail-visible1="0"
+                                 data-handle-color="#D7DCE2">
+
+                                <div class="tab-pane active" id="portlet_comments_1">
+
+                                    <!-- Seccion de todos los comentarios -->
+                                    <div class="mt-comments">
+
+                                        <!-- diseño de una fila de comentario -->
+                                        <div class="mt-comment">
+
+                                            <div class="mt-comment-img">
+                                                <img src="../assets/pages/media/users/avatar1.jpg"/></div>
+                                            <div class="mt-comment-body">
+                                                <div class="mt-comment-info">
+                                                    <span class="mt-comment-author">Leidy Ramirez</span>
+                                                    <span class="mt-comment-date">11 Agus, 10:30AM</span>
+                                                </div>
+                                                <div class="mt-comment-text"> 4 Hamburguesas Vegetarianas
+                                                </div>
+                                                <div class="mt-comment-details">
+                                                    <span class="mt-comment-status mt-comment-status-rejected">Pendiente</span>
+                                                    <ul class="mt-comment-actions">
+                                                        <li>
+                                                            <a href="#">Quick Edit</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#">View</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#">Delete</a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <!-- fin diseño de una fila de comentario -->
+
+                                        <div class="mt-comment">
+                                            <div class="mt-comment-img">
+                                                <img src="../assets/pages/media/users/avatar6.jpg"/></div>
+                                            <div class="mt-comment-body">
+                                                <div class="mt-comment-info">
+                                                    <span class="mt-comment-author">Leidy Ramirez</span>
+                                                    <span class="mt-comment-date">12 Feb, 08:30AM</span>
+                                                </div>
+                                                <div class="mt-comment-text"> 8 Carnes Fritas
+                                                </div>
+                                                <div class="mt-comment-details">
+                                                    <span class="mt-comment-status mt-comment-status-rejected">Pendiente</span>
+                                                    <ul class="mt-comment-actions">
+                                                        <li>
+                                                            <a href="#">Quick Edit</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#">View</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#">Delete</a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <!-- fin Seccion de todos los comentarios -->
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!-- fin cuerpo del div blanco PEDIDOS SIN ASIGNAR -->
+
+                </div>
+                <!-- FIN DISEÑO DE UN DIV -->
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+
+</html>
--- happyprucon/app/src/funciones.php
+++ happyprucon/app/src/funciones.php
@@ -1,259 +1,220 @@
 <?
-
 //Archivo de funciones generales que se utilizaran en la aplicacion
-
-
-
- require_once'../../externo/plugins/PDOModel.php';
+require_once '../../externo/plugins/PDOModel.php';
 // impprime las estrellas segun el promedio
-	function print_calificacion($valor)
-	{
-		$result = "";
-		$promedio=number_format($valor,1);
-		$result = "<span>";
-		for ($i = 0; $i < 5; $i++)
-		{
-			if($i < $promedio)
-			{
-				$result .= "<i class='fa fa-star' style='color:#F9F937'></i>";
-			}
-			else
-			{
-				$temp = $i - $promedio;
-				if($temp < 1 && $temp > 0)
-				{
-					$result .= "<i class='fa fa-star-half-o' style='color:#F9F937'></i>";
-				}
-				else
-				{
-					$result .= "<i class='fa fa-star-o' sstyle='color:#F9F937'></i>";
-				}
-			}
-		}
-		$result .= "</span>";
-		return $result;
-	}
+function print_calificacion($valor)
+{
+    $result = "";
+    $promedio = number_format($valor, 1);
+    $result = "<span>";
+    for ($i = 0; $i < 5; $i++) {
+        if ($i < $promedio) {
+            $result .= "<i class='fa fa-star' style='color:#F9F937'></i>";
+        } else {
+            $temp = $i - $promedio;
+            if ($temp < 1 && $temp > 0) {
+                $result .= "<i class='fa fa-star-half-o' style='color:#F9F937'></i>";
+            } else {
+                $result .= "<i class='fa fa-star-o' sstyle='color:#F9F937'></i>";
+            }
+        }
+    }
+    $result .= "</span>";
+    return $result;
+}
+
 /// calcula el promedio del emprendedor y pinta las estrellas
-	function calificacion_usuario($id_usuario)
-	{
-		$objCal = new PDOModel();
-		//consulta para extraer la suma de las filas
-		$objCal->where("id_usuario", $id_usuario);
-		$objCal->columns = array("sum(calificacion)");
-		$sumaCalificaciones =  $objCal->select("calificacion_usuario");
-		foreach ($sumaCalificaciones as $sumaCal){
-			foreach ($sumaCal as $sumCal){
-				$suma= $sumCal;
-			}
-		}
-                                          
-		//cosulta para contar el total de filas
-		$objCal->where("id_usuario", $id_usuario);
-        $objCal->columns = array("count(*) calificacion");
-        $cuentaTotal =  $objCal->select("calificacion_usuario");
-		foreach ($cuentaTotal as $cuentaTot){
-			foreach ($cuentaTot as $cuentaTo){
-				$cuenta= $cuentaTo;
-			}
-		}
-											
-		//funcion para calcular el promedio
-		if ($sumCal==0 ||$cuenta==0) 
-			{
-				$prom=0;
-			}
-											
-		else
-			{
-				$prom= $sumCal/$cuenta;
-				$promedio=number_format($prom,1);
-				echo print_calificacion($promedio);
-			}
-	}
-
-/// calcula el promedio del producto y pinta las estrellas
-	function calificacion_producto($id_producto)
-	{
-		$objCal = new PDOModel();
-		//consulta para extraer la suma de las filas
-		$objCal->where("id_producto", $id_producto);
-		$objCal->columns = array("sum(calificacion)");
-		$sumaCalificaciones =  $objCal->select("calificacion_producto");
-		foreach ($sumaCalificaciones as $sumaCal)
-		{
-			foreach ($sumaCal as $sumCal)
-			{
-				$suma= $sumCal;
-			}
-		}
-        //cosulta para contar el total de filas
-		$objCal->where("id_producto", $id_producto);
-        $objCal->columns = array("count(*) calificacion");
-        $cuentaTotal =  $objCal->select("calificacion_producto");
-		foreach ($cuentaTotal as $cuentaTot)
-		{
-			foreach ($cuentaTot as $cuentaTo)
-			{
-				$cuenta= $cuentaTo;
-			}
-		}
-		//funcion para calcular el promedio
-		if ($sumCal==0 ||$cuenta==0) 
-		{
-			$prom=0;
-		}
-		else
-		{
-			$prom= $sumCal/$cuenta;
-			$promedio=number_format($prom,1);
-			echo print_calificacion($promedio);
-		}
-	}
-
-	// funcion que trae el nombre del usuario
-
-	function nombre_usuario($id_usuario)
-	{
-		$objusu = new PDOModel();
-        $objusu->where("id", $id_usuario);
-        $objusu->columns = array("nombre_completo");
-        $usuario =  $objusu->select("usuarios");
-        foreach ($usuario as $datos)
-		{
-			foreach ($datos as $dat)
-			{
-				$nombre= $dat;
-			}
-			
-		}
-
-		return $nombre;
-
-	}
-
-/// calcula el promedio del emprendedor
-/// solo trae el valor numerico de la calificacion del empendedor
-	function calificacion_usu($id_usuario)
-	{
-		$objCal = new PDOModel();
-		//consulta para extraer la suma de las filas
-		$objCal->where("id_usuario", $id_usuario);
-		$objCal->columns = array("sum(calificacion)");
-		$sumaCalificaciones =  $objCal->select("calificacion_usuario");
-		foreach ($sumaCalificaciones as $sumaCal){
-			foreach ($sumaCal as $sumCal){
-				$suma= $sumCal;
-			}
-		}
-                                          
-		//cosulta para contar el total de filas
-		$objCal->where("id_usuario", $id_usuario);
-        $objCal->columns = array("count(*) calificacion");
-        $cuentaTotal =  $objCal->select("calificacion_usuario");
-		foreach ($cuentaTotal as $cuentaTot){
-			foreach ($cuentaTot as $cuentaTo){
-				$cuenta= $cuentaTo;
-			}
-		}
-											
-		//funcion para calcular el promedio
-		if ($sumCal==0 ||$cuenta==0) 
-			{
-				$prom=0;
-				$promedio=0;
-			}
-											
-		else
-			{
-				$prom= $sumCal/$cuenta;
-				$promedio=number_format($prom,1);
-			}
-
-			return $promedio;
-	}
-
-
-	/// calcula el promedio del producto
-	/// solo trae el valor numerico de la calificacion del producto
-	function calificacion_prod($id_producto)
-	{
-		$objCal = new PDOModel();
-		//consulta para extraer la suma de las filas
-		$objCal->where("id_producto", $id_producto);
-		$objCal->columns = array("sum(calificacion)");
-		$sumaCalificaciones =  $objCal->select("calificacion_producto");
-		foreach ($sumaCalificaciones as $sumaCal)
-		{
-			foreach ($sumaCal as $sumCal)
-			{
-				$suma= $sumCal;
-			}
-		}
-        //cosulta para contar el total de filas
-		$objCal->where("id_producto", $id_producto);
-        $objCal->columns = array("count(*) calificacion");
-        $cuentaTotal =  $objCal->select("calificacion_producto");
-		foreach ($cuentaTotal as $cuentaTot)
-		{
-			foreach ($cuentaTot as $cuentaTo)
-			{
-				$cuenta= $cuentaTo;
-			}
-		}
-		//funcion para calcular el promedio
-		if ($sumCal==0 ||$cuenta==0) 
-		{
-			$prom=0;
-			$promedio=0;
-		}
-		else
-		{
-			$prom= $sumCal/$cuenta;
-			$promedio=number_format($prom,1);
-		}
-		return $promedio;
-	}
-
-	//funcion que trae el numero total de cometarios que tiene un producto
-
-	function cantidad_coment_prod($id_producto){
-		$objComent = new PDOModel();
-		$objComent->where("id_producto", $id_producto);
-        $objComent->columns = array("count(*) comentario");
-        $cuentaTotal =  $objComent->select("calificacion_producto");
-		foreach ($cuentaTotal as $cuentaTot)
-		{
-			foreach ($cuentaTot as $cuentaTo)
-			{
-				$cuenta= $cuentaTo;
-			}
-		}
-
-		return $cuenta;
-
-	}
-
-
-function cantidad_coment_usu($id_usuario){
-    $objComent = new PDOModel();
-    $objComent->where("id_usuario", $id_usuario);
-    $objComent->columns = array("count(*) comentario");
-    $cuentaTotal =  $objComent->select("calificacion_usuario");
-    foreach ($cuentaTotal as $cuentaTot)
-    {
-        foreach ($cuentaTot as $cuentaTo)
-        {
-            $cuenta= $cuentaTo;
+function calificacion_usuario($id_usuario)
+{
+    $objCal = new PDOModel();
+    //consulta para extraer la suma de las filas
+    $objCal->where("id_usuario", $id_usuario);
+    $objCal->columns = array("sum(calificacion)");
+    $sumaCalificaciones = $objCal->select("calificacion_usuario");
+    foreach ($sumaCalificaciones as $sumaCal) {
+        foreach ($sumaCal as $sumCal) {
+            $suma = $sumCal;
         }
     }
 
-    return $cuenta;
+    //cosulta para contar el total de filas
+    $objCal->where("id_usuario", $id_usuario);
+    $objCal->columns = array("count(*) calificacion");
+    $cuentaTotal = $objCal->select("calificacion_usuario");
+    foreach ($cuentaTotal as $cuentaTot) {
+        foreach ($cuentaTot as $cuentaTo) {
+            $cuenta = $cuentaTo;
+        }
+    }
 
+    //funcion para calcular el promedio
+    if ($sumCal == 0 || $cuenta == 0) {
+        $prom = 0;
+    } else {
+        $prom = $sumCal / $cuenta;
+        $promedio = number_format($prom, 1);
+        echo print_calificacion($promedio);
+    }
+}
+
+/// calcula el promedio del producto y pinta las estrellas
+function calificacion_producto($id_producto)
+{
+    $objCal = new PDOModel();
+    //consulta para extraer la suma de las filas
+    $objCal->where("id_producto", $id_producto);
+    $objCal->columns = array("sum(calificacion)");
+    $sumaCalificaciones = $objCal->select("calificacion_producto");
+    foreach ($sumaCalificaciones as $sumaCal) {
+        foreach ($sumaCal as $sumCal) {
+            $suma = $sumCal;
+        }
+    }
+    //cosulta para contar el total de filas
+    $objCal->where("id_producto", $id_producto);
+    $objCal->columns = array("count(*) calificacion");
+    $cuentaTotal = $objCal->select("calificacion_producto");
+    foreach ($cuentaTotal as $cuentaTot) {
+        foreach ($cuentaTot as $cuentaTo) {
+            $cuenta = $cuentaTo;
+        }
+    }
+    //funcion para calcular el promedio
+    if ($sumCal == 0 || $cuenta == 0) {
+        $prom = 0;
+    } else {
+        $prom = $sumCal / $cuenta;
+        $promedio = number_format($prom, 1);
+        echo print_calificacion($promedio);
+    }
+}
+
+// funcion que trae el nombre del usuario
+function nombre_usuario($id_usuario)
+{
+    $objusu = new PDOModel();
+    $objusu->where("id", $id_usuario);
+    $objusu->columns = array("nombre_completo");
+    $usuario = $objusu->select("usuarios");
+    foreach ($usuario as $datos) {
+        foreach ($datos as $dat) {
+            $nombre = $dat;
+        }
+
+    }
+    return $nombre;
+}
+
+/// calcula el promedio del emprendedor
+/// solo trae el valor numerico de la calificacion del empendedor
+function calificacion_usu($id_usuario)
+{
+    $objCal = new PDOModel();
+    //consulta para extraer la suma de las filas
+    $objCal->where("id_usuario", $id_usuario);
+    $objCal->columns = array("sum(calificacion)");
+    $sumaCalificaciones = $objCal->select("calificacion_usuario");
+    foreach ($sumaCalificaciones as $sumaCal) {
+        foreach ($sumaCal as $sumCal) {
+            $suma = $sumCal;
+        }
+    }
+
+    //cosulta para contar el total de filas
+    $objCal->where("id_usuario", $id_usuario);
+    $objCal->columns = array("count(*) calificacion");
+    $cuentaTotal = $objCal->select("calificacion_usuario");
+    foreach ($cuentaTotal as $cuentaTot) {
+        foreach ($cuentaTot as $cuentaTo) {
+            $cuenta = $cuentaTo;
+        }
+    }
+
+    //funcion para calcular el promedio
+    if ($sumCal == 0 || $cuenta == 0) {
+        $prom = 0;
+        $promedio = 0;
+    } else {
+        $prom = $sumCal / $cuenta;
+        $promedio = number_format($prom, 1);
+    }
+    return $promedio;
+}
+
+/// calcula el promedio del producto
+/// solo trae el valor numerico de la calificacion del producto
+function calificacion_prod($id_producto)
+{
+    $objCal = new PDOModel();
+    //consulta para extraer la suma de las filas
+    $objCal->where("id_producto", $id_producto);
+    $objCal->columns = array("sum(calificacion)");
+    $sumaCalificaciones = $objCal->select("calificacion_producto");
+    foreach ($sumaCalificaciones as $sumaCal) {
+        foreach ($sumaCal as $sumCal) {
+            $suma = $sumCal;
+        }
+    }
+    //cosulta para contar el total de filas
+    $objCal->where("id_producto", $id_producto);
+    $objCal->columns = array("count(*) calificacion");
+    $cuentaTotal = $objCal->select("calificacion_producto");
+    foreach ($cuentaTotal as $cuentaTot) {
+        foreach ($cuentaTot as $cuentaTo) {
+            $cuenta = $cuentaTo;
+        }
+    }
+    //funcion para calcular el promedio
+    if ($sumCal == 0 || $cuenta == 0) {
+        $prom = 0;
+        $promedio = 0;
+    } else {
+        $prom = $sumCal / $cuenta;
+        $promedio = number_format($prom, 1);
+    }
+    return $promedio;
+}
+
+//funcion que trae el numero total de cometarios que tiene un producto
+function cantidad_coment_prod($id_producto)
+{
+    $objComent = new PDOModel();
+    $objComent->where("id_producto", $id_producto);
+    $objComent->columns = array("count(*) comentario");
+    $cuentaTotal = $objComent->select("calificacion_producto");
+    foreach ($cuentaTotal as $cuentaTot) {
+        foreach ($cuentaTot as $cuentaTo) {
+            $cuenta = $cuentaTo;
+        }
+    }
+    return $cuenta;
+}
+
+function cantidad_coment_usu($id_usuario)
+{
+    $objComent = new PDOModel();
+    $objComent->where("id_usuario", $id_usuario);
+    $objComent->columns = array("count(*) comentario");
+    $cuentaTotal = $objComent->select("calificacion_usuario");
+    foreach ($cuentaTotal as $cuentaTot) {
+        foreach ($cuentaTot as $cuentaTo) {
+            $cuenta = $cuentaTo;
+        }
+    }
+    return $cuenta;
 }
 
 
+function nombre_producto($id_producto)
+{
+    $objComent = new PDOModel();
+    $objComent->where("id", $id_producto);
+    $objComent->columns = array("nombre");
+    $cuentaTotal = $objComent->select("producto");
+    foreach ($cuentaTotal as $cuentaTot) {
 
-
-
-
+        $cuenta = $cuentaTot["nombre"];
+    }
+    return $cuenta;
+}
 ?>
