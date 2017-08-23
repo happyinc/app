c7f9648b60e75ff3ab323154d21b0ae005fbebce
DESARROLLO HAPPY INC <happy09127@gmail.com>
2017-08-23 10:23:30 -0500

    pdo

--- happyprucon/externo/plugins/PDOModel.php
+++ happyprucon/externo/plugins/PDOModel.php
@@ -16,13 +16,6 @@
   ---------------------------------------------------------------------------------------------------------------
  */
 
-function AbrirConexionHappy()
-{
-
-    $conexion = mysql_connect("happyhappyinc.com","bd_happy", "bd_happy2017*");
-    mysql_select_db("bd_happy", $conexion);
-    return $conexion;
-}
 Class PDOModel {
 
     public $columns = "*";              //columns of table
